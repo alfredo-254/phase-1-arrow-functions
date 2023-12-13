ab4df0bbce9b7321bbbf4f34bb1938bab0bfc6ff
alfred kivuva <kivuvakioko97@gmail.com>
2023-12-14 00:44:43 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+// describe("index.js", () => {
+//     it("has a function expression called divide", () => {
+//       expect(divide).to.exist
+//     })
+  
+//     it("divide divides 2000 by 100", () => {
+//       expect(divide()).to.equal(20)
+//     })
+  
+//     it("has an arrow function called square", () => {
+//       expect(square).to.exist
+//     })
+  
+//     it("square arrow function takes one parameter and multiplies it times itself", () => {
+//       let x = 2
+//       expect(square(x)).to.equal(4)
+//     })
+  
+//     it("has an arrow function called add", () => {
+//       expect(add).to.exist
+//     })
+  
+//     it("add arrow function takes two parameters and adds them together", () => {
+//       let a = 3
+//       let b = 4
+//       expect(add(a,b)).to.equal(a+b)
+//     })
+//   })
+  
+const divide=(numerator, denominator)=>numerator/denominator;
+
+const square=(number)=>number*number;
+
+const add=(num1,num2)=>num1+num2;
